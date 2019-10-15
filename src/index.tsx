d05965e442fc5a451025f4fd0a5bea90c2f3b499
DASILVA Ludovic <ldasilva@easyvista.com>
2019-10-15 17:09:19 +0200

    Revert index.tsx changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Notifier from './app/components/notifier/notifier.component';
-// import * as serviceWorker from './serviceWorker';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const data = { key: 123456};
-
-ReactDOM.render(
-  <Notifier title="Hello World" age="42" user={data}>
-    <input />
-  </Notifier>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.unregister();
