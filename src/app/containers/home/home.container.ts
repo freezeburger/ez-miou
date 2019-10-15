13ecc7a80f683fee43870f9c8a1a827c14928d59
freezeburger <renaud.dubuis@decryptage.net>
2019-10-15 16:09:49 +0200

    containers

---
+++ src/app/containers/home/home.container.ts
@@ -0,0 +1,19 @@
+import React from 'react';
+
+ class Home extends React.Component{
+     
+    constructor(props:any){
+         super(props);
+     }
+
+     //LifeCycle
+     componentDidMount(){}
+
+     componentWillUnmount(){}
+
+     render(){
+         return '<div>Home</div>'
+     }
+ }
+
+
