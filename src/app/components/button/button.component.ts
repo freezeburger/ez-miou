2e6d2ed24289e366821e489c8cf243cd6b361b9c
freezeburger <renaud.dubuis@decryptage.net>
2019-10-15 16:10:41 +0200

    Button Component

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,19 @@
+import React from 'react';
+
+ class Button extends React.Component{
+     
+    constructor(props:any){
+         super(props);
+     }
+
+     //LifeCycle
+     componentDidMount(){}
+
+     componentWillUnmount(){}
+
+     render(){
+         return '<div>Button</div>'
+     }
+ }
+
+
