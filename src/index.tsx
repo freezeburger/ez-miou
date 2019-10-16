11364f79592c4c10e40081205e07c19d08644714
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2019-10-16 15:30:21 +0200

    Removed unused

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './app/containers/home/home.container';
-import * as serviceWorker from './serviceWorker';
-import Button from './app/components/button/button.component';
-import Me from './app/components/me/me.component';
-import Avatar from './app/components/avatar/avatar.component';
-import { FaBeer } from 'react-icons/fa';
 import RoomCard from './app/components/room-card/roomcard.component';
 
 const data = {
@@ -15,10 +9,9 @@ const data = {
 };
 
 ReactDOM.render(
-    <div>
-        <RoomCard ></RoomCard>
-    </div>
-  ,
+  <div>
+    <RoomCard></RoomCard>
+  </div>,
   document.getElementById('root')
 );
 
