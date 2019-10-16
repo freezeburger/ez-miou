0f153ad2176b5301564d97ceae4c8ad81a6bf372
Christophe Daniel <cdaniel@easyvista.com>
2019-10-16 10:23:27 +0200

    props room card

--- src/app/components/room-card/roomcard.component.tsx
+++ src/app/components/room-card/roomcard.component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import RoomCardProps from '../@prop-types/roomcard.props';
 
-const RoomCard = (props: any) => {
+const RoomCard = (props: RoomCardProps) => {
     return (
         <div>RoomCard</div>
     );
