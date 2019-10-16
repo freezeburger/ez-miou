1288dd9b6a7651ddf195629c9d85a7077381fa4f
Christophe Daniel <cdaniel@easyvista.com>
2019-10-16 12:04:42 +0200

    update app user

--- src/app/services/@types/app-user.ts
+++ src/app/services/@types/app-user.ts
@@ -1,16 +1,19 @@
+/**
+ * enum App User Status
+ */
 export enum AppUserStatus {
-    ONLINE = 'AppUserStatus-ONLINE',
-    OFFLINE = 'AppUserStatus-OFFLINE',
-    BUSY = 'AppUserStatus-BUSY',
-    AWAY = 'AppUserStatus-AWAY'
+  ONLINE = "AppUserStatus-ONLINE",
+  OFFLINE = "AppUserStatus-OFFLINE",
+  BUSY = "AppUserStatus-BUSY",
+  AWAY = "AppUserStatus-AWAY"
 }
 
 interface AppUser {
-    id:number;
-    name: string;
-    email:string;
-    avatarUrl?: string;
-    status: AppUserStatus;
+  id: number;
+  name: string;
+  email: string;
+  avatarUrl?: string;
+  status: AppUserStatus;
 }
 
 export default AppUser;
