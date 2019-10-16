c69bdf9da9eff50b0037d7a89c55e55b8bf48f21
Christophe Daniel <cdaniel@easyvista.com>
2019-10-16 17:15:25 +0200

    room card cursor

--- src/app/components/room-card/roomcard.component.tsx
+++ src/app/components/room-card/roomcard.component.tsx
@@ -3,18 +3,22 @@ import RoomCardProps from "../@prop-types/room-card.props";
 import defaultRoomCardProps from "./room-card.default-props";
 
 const RoomCard = (props: RoomCardProps) => {
+  let clickable: boolean = !!props.action;
   /**
    * Function will handle click
    */
   const handleClick = () => {
     if (!!props.action) {
-        props.action();
+      props.action();
     }
-    
   };
 
   return (
-    <div className="card" onClick={handleClick}>
+    <div
+      className="card"
+      onClick={handleClick}
+      style={{ cursor: clickable ? "pointer" : "default" }}
+    >
       <div className="card-body" style={{ border: "1px solid " + props.color }}>
         <h5 className="card-title">{props.title}</h5>
         <div>
