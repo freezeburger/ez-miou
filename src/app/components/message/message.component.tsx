fe2000192a2306763f16205ff076c9812aa55b7b
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2019-10-16 16:33:51 +0200

    removed useless constructor

--- src/app/components/message/message.component.tsx
+++ src/app/components/message/message.component.tsx
@@ -3,10 +3,6 @@ import MessageProps from '../@prop-types/message.props';
 import ReactMarkdown from 'react-markdown';
 
 export default class Message extends React.Component<MessageProps, any> {
-  constructor(props: MessageProps) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="message">
