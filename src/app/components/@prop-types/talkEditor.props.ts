0039e9db53cbf5a4d2057529d7249fd559e08e39
Raphael <domingues-raphael@live.fr>
2019-10-16 10:17:13 +0200

    Room Cloud
    Add interfaces to room cloud

---
+++ src/app/components/@prop-types/talkEditor.props.ts
@@ -0,0 +1,7 @@
+/**
+ * Talk editor props interface
+ */
+export default interface RoomCloudProps {
+    userList: any; // List of users
+    emojiList: any; // List of emoji
+} 
---
+++ src/app/components/talk-editor/talkEditor.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TalkEditor = (props: any) => {
+    return (
+        <div>TalkEditor</div>
+    );
+};
+
+
