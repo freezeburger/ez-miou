69c6ae40c52597466cdea9fd2ac26742195ca903
Didier Demange <ddemange@easyvista.com>
2019-10-16 11:24:33 +0200

    Move default export to the bottom of the file

--- src/app/components/@prop-types/room-creator.props.ts
+++ src/app/components/@prop-types/room-creator.props.ts
@@ -2,10 +2,12 @@
  * Defines the props expected by the RoomCreator component.
  * @see RoomCreator
  */
-export default interface RoomCreatorProps {
+interface RoomCreatorProps {
     /**
      * Function called when the room data is validated
      * and the room can be created.
      */
     onCreationRequested(roomName: string): Function;
 }
+
+export default RoomCreatorProps;
