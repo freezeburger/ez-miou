49ae94e1a619d344b0bb0b80f13ad50d9e9c455d
Christophe Daniel <cdaniel@easyvista.com>
2019-10-17 10:12:03 +0200

    room card stories

---
+++ src/stories/room-card.stories.tsx
@@ -0,0 +1,28 @@
+import React, { CSSProperties } from "react";
+import { action } from "@storybook/addon-actions";
+import { default as Welcome } from "../app/components/me/me.component";
+import { storiesOf } from "@storybook/react";
+import { withKnobs, text, boolean, number, color } from "@storybook/addon-knobs";
+import { withMarkdownNotes } from "@storybook/addon-notes";
+import RoomCard from "../app/components/room-card/roomcard.component";
+
+/* Component wrapper */
+const styles: CSSProperties = {
+  textAlign: "center"
+};
+const Wrapper = ({ children }: any) => <div style={styles}>{children}</div>;
+
+/* Storie Title */
+storiesOf("Room Card", module)
+  .addDecorator(withKnobs)
+  /* Sub Title */
+  .add("Try The Knobs", () => (
+    <Wrapper>
+      <RoomCard 
+        title={text("Title Room", "my room")}
+        color={color("Border Color", "green")}
+        action={action("handleClick")}
+        user={}
+      ></RoomCard>
+    </Wrapper>
+  ));
