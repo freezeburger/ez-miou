c38a2907777f3c5487e11ef4f7376a16d532d976
Constant KOBANA <ckobana@easyvista.com>
2019-10-17 14:30:26 +0200

    creationde ma fure home

---
+++ src/app/features/home/home.feature.ts
@@ -0,0 +1,17 @@
+import React from 'react';
+
+ class Home extends React.Component{
+     
+    constructor(props:any){
+         super(props);
+     }
+
+     //LifeCycle
+     componentDidMount(){}
+
+     componentWillUnmount(){}
+
+     render(){
+         return '<div>Home</div>'
+     }
+ }
