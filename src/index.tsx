37598a6455718cfc8477bf8d082996024cac2f68
younes-barhouni <y.barhouni@gmail.com>
2019-10-17 11:42:28 +0200

    fix conflict

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,6 @@ const data = {
   key: 123456
 };
 
-<<<<<<< HEAD
 ReactDOM.render(
   <div>
     <UserProfile></UserProfile>
@@ -21,9 +20,6 @@ ReactDOM.render(
   </div>,
   document.getElementById('root')
 );
-=======
-ReactDOM.render(<div>#paz sur vous les chofaent qui suppriment index #chofa #gestapo</div>, document.getElementById('root'));
->>>>>>> 68a9bac00a312c5ba2898aba926b4c421efc32b5
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
