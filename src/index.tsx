7c01f0d5d03973a0fc4f185570086ecc0908ae4e
Didier Demange <ddemange@easyvista.com>
2019-10-17 13:57:49 +0200

    Remove useless imports

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import RoomCard from './app/components/room-card/roomcard.component';
 import UserProfile from './app/components/user-profile/userprofile.component';
-import Avatar from './app/components/avatar/avatar.component';
-import Message from './app/components/message/message.component';
-import Button from './app/components/button/button.component';
 import RoomCloud from './app/components/room-cloud/room-cloud.component';
 
 const data = {
