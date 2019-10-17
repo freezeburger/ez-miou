2e22ebf0e121a39bcb2e3548a38112249fb6328a
Cyriaque DELAUNAY <cyriaquedelaunay@gmail.com>
2019-10-17 15:31:54 +0200

    updated index

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,29 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import UserProfile from './app/components/user-profile/userprofile.component';
 import RoomCloud from './app/components/room-cloud/room-cloud.component';
+import MessageList from './app/components/message-list/messagelist.component';
+import './app.scss';
+import RoomSearch from './app/components/room-search/room-search.component';
+import ImageSlider from './app/components/image-slider/image-slider.component';
 
 ReactDOM.render(
-  <div>
-    <RoomCloud/>
-    <UserProfile></UserProfile>
-
+  <div className="app">
+    <div className="container-fluid ">
+      <div className="row">
+        <div className="col-3">
+          <UserProfile />
+        </div>
+        <div className="col-9">
+          <div className="row">{/* <RoomCloud /> */}</div>
+          <div className="row">
+            <MessageList />
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <ImageSlider />
+      </div>
+    </div>
   </div>,
   document.getElementById('root')
 );
