307789f2c6f31c0da204c6e00a94a38cc273105d
Norbert Mauger <norbertmauger@gmail.com>
2019-10-17 10:33:17 +0200

    ImageSlider: added imageUrl knob

--- src/stories/2-image-slider.stories.tsx
+++ src/stories/2-image-slider.stories.tsx
@@ -6,6 +6,7 @@ import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import { withMarkdownNotes} from '@storybook/addon-notes';
 import ImageSlider from '../app/components/image-slider/image-slider.component';
 import { bool } from 'prop-types';
+import defaultProps from '../app/components/image-slider/image-slider.default-props';
 
 
 /* Component wrapper */
@@ -23,7 +24,11 @@ storiesOf('ImageSlider', module)
       <ImageSlider 
       delay={number('Display Time in ms', 1500)}
       loop={boolean('loop or back', true)}
+      imageList={[text('Pic 1', defaultProps.imageList[0]),
+                  text('Pic 2', defaultProps.imageList[1]),
+                  text('Pic 3', defaultProps.imageList[2])]
+      }
       ></ImageSlider>
     </Wrapper>
   ),
-  { notes:'A Simple Comment Note'})
+  { notes:'Not yet finished... '})
